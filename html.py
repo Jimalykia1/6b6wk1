4e621b7b82c0b63b8894e75045d99fe9a6db5e31
jimalykia1 <gweempire@gmail.com>
2023-10-04 16:47:38 +0100

    modify

---
+++ html.py
@@ -0,0 +1,23 @@
+# # # print("welcome to backend class")
+# # # print("hello 6b6")
+# # # print(type("hello6b6"))
+# # # print(type(3))
+# # # print(type(9.4))
+# # # print(type("2"))
+# # # print(1000000)
+# # name="jim"
+# # print(name)
+# # # age="18"
+# # print(18)
+# # print("Hello,your name is" + name +",you are" +age + "years old")
+# print("2+2")
+# print(2+2)
+# print("2"+"2")
+# print(9/2)
+# print(9//2)
+# greet='Hello'
+
+# print(greet[1:4])
+
+fruit="banana "
+print(len(fruit))
